86eb71255cb41af2387ec6531ef8cae1574eccbf
teknopants <teknopants@gmail.com>
2016-01-30 15:54:47 -0800

    cuttlefish

---
+++ Assets/CuttleFish.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CuttleFish : MonoBehaviour {
+
+	private Rigidbody myBod;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		myBod = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 moveVector = target.transform.position - transform.position;
+		//moveVector += Vector3(Random.value*2,Random.value*2,Random.value*2);
+		moveVector.Normalize();
+		myBod.AddForce(moveVector,ForceMode.VelocityChange);
+	}
+}
